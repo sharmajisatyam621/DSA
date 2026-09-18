a7cd615ebebd1999bfea6af6a8a1d5d2c449bbf4
Satyam Sharma <sharmajisatyam621@gmail.com>
2026-09-18 23:09:04 +0530

    Time: 11 ms (65.28%), Space: 23.4 MB (51.15%) - LeetHub

---
+++ 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    vector<string> maxNumOfSubstrings(string s) {
+        int n = s.size();
+
+        vector<int> first(26, n);
+        vector<int> last(26, -1);
+
+        // Find first and last occurrence of every character
+        for (int i = 0; i < n; i++) {
+            int x = s[i] - 'a';
+
+            first[x] = min(first[x], i);
+            last[x] = i;
+        }
+
+        vector<pair<int, int>> intervals;
+
+        // Build the smallest valid interval for each character
+        for (int c = 0; c < 26; c++) {
+            if (last[c] == -1)
+                continue;
+
+            int l = first[c];
+            int r = last[c];
+
+            bool valid = true;
+
+            for (int i = l; i <= r; i++) {
+                int x = s[i] - 'a';
+
+                // This character has an occurrence
+                // before our current left boundary
+                if (first[x] < l) {
+                    valid = false;
+                    break;
+                }
+
+                // Need to include all occurrences of x
+                r = max(r, last[x]);
+            }
+
+            if (valid)
+                intervals.push_back({l, r});
+        }
+
+        // Select maximum number of non-overlapping intervals
+        sort(intervals.begin(), intervals.end(),
+             [](const pair<int,int>& a,
+                const pair<int,int>& b) {
+                 return a.second < b.second;
+             });
+
+        vector<string> ans;
+        int prevEnd = -1;
+
+        for (auto [l, r] : intervals) {
+            if (l > prevEnd) {
+                ans.push_back(s.substr(l, r - l + 1));
+                prevEnd = r;
+            }
+        }
+
+        return ans;
+    }
+};
